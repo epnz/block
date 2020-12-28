ab5d19182e4d10d754381412a24121c0040da2e4
epnz <g@epnz.com>
2020-12-28 19:11:20 +0800

    Test Composer autoload files

---
+++ block.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+ * @Author: 故乡情
+ * @Date: 2020-12-28 18:57:11
+ * @LastEditTime: 2020-12-28 18:57:11
+ * @LastEditors: 故乡情
+ * @Description: EPower Network Zealot Project Block
+ * @FilePath: /block/block.php
+ * @Copyright © 2020 EPNZ.com
+ * @但请保留版权信息
+ */
+
+namespace epnz\block;
+
+if (!function_exists('test')) {
+    /**
+     * Test.
+     *
+     * @param mixed $value
+     * @return Collection
+     */
+    function test()
+    {
+        return '这个 Composer 我还不会，这只是一个测试';
+    }
+}
