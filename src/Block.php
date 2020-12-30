23e08c2298fe97e09003bb3b6b2d841368aa92a6
epnz <g@epnz.com>
2020-12-30 16:09:31 +0800

    Mixed use of implementation methods and static methods

--- src/Block.php
+++ src/Block.php
@@ -2,7 +2,7 @@
 /* 
  * @Author: 故乡情
  * @Date: 2020-12-28 18:57:11
- * @LastEditTime: 2020-12-29 18:53:56
+ * @LastEditTime: 2020-12-30 16:09:20
  * @LastEditors: 故乡情
  * @Description: EPower Network Zealot Project Block
  * @FilePath: /block/src/block.php
@@ -17,7 +17,7 @@ class Block
     /**
      * Version
      */
-    const VERSION = '0.0.6';
+    const VERSION = '0.0.7';
 
     protected $lang = 'zh-cn';
 
@@ -29,14 +29,14 @@ class Block
 
     public $class;
 
+    /**
+     * @description:  前置操作
+     * @param   array $paths
+     * @return  null
+     */
     public function __construct($paths = [])
     {
-        spl_autoload_register(function ($class) {
-            $class = ltrim($class, 'epnz\\');
-            include $class . '.php';
-        });
-        
-        if(empty($paths)){
+        if (empty($paths)) {
             $this->rootPath = dirname(__DIR__);
             //$this->rootPath = dirname(__DIR__, 4);
         } else {
@@ -45,26 +45,51 @@ class Block
 
         $this->thisPath = __DIR__;
 
-        if(isset($paths['config'])){
-            $basic = $paths['config'] . DIRECTORY_SEPARATOR .'basic.php';
-            if(file_exists($basic)){
+        if (isset($paths['config'])) {
+            $basic = $paths['config'] . DIRECTORY_SEPARATOR . 'basic.php';
+            if (file_exists($basic)) {
                 $basic = require_once $basic;
                 $this->lang = $basic['language'];
             }
         }
 
-        $blockLanguagePath = $this->thisPath . DIRECTORY_SEPARATOR 
-        . 'language' . DIRECTORY_SEPARATOR . $this->lang . DIRECTORY_SEPARATOR;
+        $blockLanguagePath = $this->thisPath . DIRECTORY_SEPARATOR
+            . 'language' . DIRECTORY_SEPARATOR . $this->lang . DIRECTORY_SEPARATOR;
 
         $http = require_once $blockLanguagePath . 'http.php';
 
         $this->httpCode = $http;
         $this->class = __CLASS__;
-        
-        // $autoloadFile = ['default'];
+    }
 
-        // foreach ($autoloadFile as $v) {
-        //     require_once $v . '.php';
-        // }
+    /**
+     * @description: 实现非静态方法
+     * "..."语法实现支持可变数量的参数列表
+     */
+    public function __call($method, $parameters)
+    {
+        if (in_array($method, $this->methods)) {
+            return $this->$method(...$parameters);
+        }
+        return 'No method: ' . $method . PHP_EOL;
+    }
+
+    /**
+     * @description: 实现静态方法
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        return (new static)->$method(...$parameters);
     }
 }
+
+// 实现方法和文件的自动加载
+spl_autoload_register(function ($class) {
+    $class = ltrim($class, 'epnz\\');
+    $file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
+    if (!file_exists($file)) {
+        trigger_error('Trigger a fatal error', E_USER_ERROR);
+        exit();
+    }
+    include_once $file;
+});
--- src/Message.php
+++ src/Message.php
@@ -2,22 +2,26 @@
 /*
  * @Author: 故乡情
  * @Date: 2020-12-29 05:41:58
- * @LastEditTime: 2020-12-29 18:01:35
+ * @LastEditTime: 2020-12-30 14:40:33
  * @LastEditors: 故乡情
  * @Description: EPower Network Zealot Project Block
- * @FilePath: \block\src\Message.php
+ * @FilePath: /block/src/Message.php
  * @Copyright © 2020 EPNZ.com
  * 请保留版权信息
  */
 
 namespace epnz;
 
-use epnz\block;
+use epnz\Block;
 
-class msg extends Block
+class Message extends Block
 {
-    public function test()
+    protected $methods = [
+        'test'
+    ];
+    
+    protected function test()
     {
-        return 'MSG';
+        return '--MSG--' . PHP_EOL;
     }
 }
