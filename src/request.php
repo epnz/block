12b7f81e63cba4eae99d618a9ca4d95a2cb71960
epnz <g@epnz.com>
2021-01-03 23:03:13 +0800

    git config core.ignorecase false

---
+++ src/request.php
@@ -0,0 +1,60 @@
+<?
+/*
+ * @Author: 故乡情
+ * @Date: 2020-12-29 17:55:15
+ * @LastEditTime: 2021-01-02 23:01:55
+ * @LastEditors: 故乡情
+ * @Description: EPower Network Zealot Project Block
+ * @FilePath: /block/src/request.php
+ * @Copyright © 2020 EPNZ.com
+ * 请保留版权信息
+ */
+
+namespace block;
+
+class request
+{
+    /**
+     * @description: 实现的方法
+     */
+    protected $methods = [
+        'curlPost'
+    ];
+
+    private $requestMethod = [
+        'GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE'
+    ];
+  
+    
+    /**
+     * @description: CURL in POST
+     * @access  public
+     * @param   string  $url    URL
+     * @param   array   $param  参数
+     * @return  string
+     */
+    public function curlPost($url, $param = null)
+    {
+        if (is_array($param)) {
+            $param = http_build_query($param);
+        }
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+
+        // 抓取URL并把它传递给浏览器
+        $raw = curl_exec($ch);
+
+        // 关闭cURL资源，并且释放系统资源
+        curl_close($ch);
+
+        return $raw;
+    }
+}
